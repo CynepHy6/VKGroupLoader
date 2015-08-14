40aa15731cc55501902b18654af750225fc0a47c
CynepHy6@github.com <Djljghjdjl13>
2015-08-14 23:59:38 +0300

    fix behavior delete button

--- app/src/main/java/ru/work/hy6/simplecamerawork/ListFilesActivity.kt
+++ app/src/main/java/ru/work/hy6/simplecamerawork/ListFilesActivity.kt
@@ -14,18 +14,17 @@ import java.io.File
 import java.io.FileFilter
 import kotlin.properties.Delegates
 
-public class ListFilesActivity() : Activity(), View.OnClickListener{
-    private val A_TEXT = "text"
-    private val A_IMAGE = "image"
-    private val A_CHECK = "checkbox"
+public class ListFilesActivity() : Activity(), View.OnClickListener {
 
     private val DIRECTORY = activeDirectory
-    private val FILES: Array<out File> by Delegates.lazy{
-        DIRECTORY.listFiles(FileFilter{!it.isDirectory()})
+    private val FILES: Array<out File> by Delegates.lazy {
+        DIRECTORY.listFiles(FileFilter { !it.isDirectory() })
     }
-    private val NAMES: Array<String> by Delegates.lazy{
+    private val NAMES: Array<String> by Delegates.lazy {
         Array(FILES.size(), { i -> "${FILES[i].name} \t ${FILES[i].length() / 1024} Kb" })
     }
+
+    private var isItemСheck = false
 
 
     // TODO: CustomAdapter<Array<Bitmap>>
@@ -44,17 +43,33 @@ public class ListFilesActivity() : Activity(), View.OnClickListener{
 
         lvListFiles.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE)
         lvListFiles.setAdapter(adapter)
-
         tvListFilesTitle.setText(DIRECTORY.getAbsolutePath())
         bDelete.setOnClickListener(this)
+        lvListFiles.setOnItemClickListener(object : AdapterView.OnItemClickListener {
+            override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                log("view.width: ${view.getWidth()}, view.id: ${view.getId()}, pos: ${position}, id: ${id}")
+                isItemСheck = false
+                if (FILES.size() > 0) {
+                    val sba = lvListFiles.getCheckedItemPositions()
+                    for (i in 0..FILES.size() - 1) {
+                        val key = sba.keyAt(i)
+                        if (sba.get(key)) {
+                            isItemСheck = true
+                            break
+                        }
+                    }
+                }
+                updateLabelButton()
+            }
+        })
         updateLabelButton()
     }
 
     private fun updateLabelButton() {
-        if (FILES.size() == 0){
-            bDelete.setText(R.string.label_back_button)
-        }else{
+        if (isItemСheck) {
             bDelete.setText(R.string.label_delete_button)
+        } else {
+            bDelete.setText(R.string.label_back_button)
         }
     }
 
@@ -62,16 +77,22 @@ public class ListFilesActivity() : Activity(), View.OnClickListener{
     override fun onClick(v: View) {
         when (v) {
             bDelete -> {
-                val sba = lvListFiles.getCheckedItemPositions()
-
-                for (i in 0..FILES.size()-1) {
-                    val key = sba.keyAt(i)
-                    if (sba.get(key))
-                    log("${FILES[key].name}, delete: ${FILES[key].delete()}") //
-                }
-                finish()
+                actionDelete()
             }
         }
+    }
+
+    private fun actionDelete() {
+        val sba = lvListFiles.getCheckedItemPositions()
+        if (isItemСheck) {
+            for (i in 0..FILES.size() - 1) {
+                val key = sba.keyAt(i)
+                if (sba.get(key))
+                    log("${FILES[key].name}, delete: ${FILES[key].delete()}")
+
+            }
+        }
+        finish()
     }
 
 }
